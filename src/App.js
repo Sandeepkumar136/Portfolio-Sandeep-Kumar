9a378e3354050176c1455ad664766fc61c1147ee
Sandeep <Ksundip750@gmail.com>
2025-08-14 23:41:37 +0530

    Add Home component and typing animation for landing page
    - Created Home component to serve as the main landing page.
    - Implemented HomeTyping component for animated typing effect showcasing various developer roles.
    - Updated App.js to include Home component.
    - Enhanced Style.css and Style.scss for improved styling and responsiveness.
    - Added animations using Framer Motion for a dynamic user experience.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from './components/navigation/Navbar';
 import './components/styles/Style.css';
+import Home from './components/Routes/Home';
 
 const App = () => {
   return (
     <div>
       <Navbar/>
+      <Home/>
     </div>
   )
 }
