d96df76ad6a37ac90983a03ac726edee9ded00d3
Sandeep <Ksundip750@gmail.com>
2025-08-25 00:04:20 +0530

    Refactor code structure for improved readability and maintainability

--- src/components/Routes/Books.js
+++ src/components/Routes/Books.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import Image_Exported from '../assets/ImageExporter'
 
 const Books = () => {
   return (
-    <div>
-      Books
+    <div className='b-container'>
+      <div className="b-contain">
+        <h3 className="heading-b-n">Stories Brewing Soon</h3>
+        <p className="subtitle-b-n">A fresh collection of untold tales, slowly crafted to inspire, entertain, and spark imagination worldwide.</p>
+      </div>
+      <img src={Image_Exported['Book-not-found']} itemType='svg' alt="Book not Found" className="img-b-n" />
+      
     </div>
   )
 }
--- src/components/assets/ImageExporter.js
+++ src/components/assets/ImageExporter.js
@@ -2,5 +2,6 @@ const Image_Exported = {
     "profile_picture": require('./images/IMG-20250414-WA0006.jpg'),
     "user-picture": require('./images/gare.PNG'),
     "madel-image": require('./images/star medal.png'),
+    "Book-not-found": require('./images/vecteezy_young-man-painting-on-easel_69294864.png'),
 }
 export default Image_Exported;
